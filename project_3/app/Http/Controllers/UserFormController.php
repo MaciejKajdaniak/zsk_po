d45ecd53e4881d1e5f478e9e7c0064677c35d844
MaciejKajdaniak <kajdaniak.mk@gmail.com>
2023-11-08 14:03:24 +0100

    add new file

--- project_3/app/Http/Controllers/UserFormController.php
+++ project_3/app/Http/Controllers/UserFormController.php
@@ -18,6 +18,20 @@ class UserFormController extends Controller
             'firstName' => 'required | min:3 | max:10',
             'lastName' => 'required | min:3 | max:20',
             'mail' => 'required | min:3 | max:10 | email',
+        ],
+        [
+            //'firstName.required' => 'Wypełnij pole :attribute',
+            //'firstName.min' => 'Wpisz conajmniej :min znaki do pola :attribute',
+            //'firstName.max' => 'Możeswz wpisac maksymalnie :max znaków do pola :attribute',
+
+            //'lastName.required' => 'Wypełnij pole :attribute',
+            //'lastName.min' => 'Wpisz conajmniej :min znaki do pola :attribute',
+            //'lastName.max' => 'Możeswz wpisac maksymalnie :max znaków do pola :attribute',
+
+            //'mail.required' => 'Wypełnij pole :attribute',
+            //'mail.min' => 'Wpisz conajmniej :min znaki do pola :attribute',
+            //'mail.max' => 'Możeswz wpisac maksymalnie :max znaków do pola :attribute',
+            //'mail.email' => 'Pole :attribute musi być typu email',
         ]);
         
         $data =[
